a325ae1025f26ca265648a6900fb723b52bfe11a
tp1de <tp1degit@gmail.com>
2021-12-29 15:35:40 +0100

    v0.9.9

--- admin/words.js
+++ admin/words.js
@@ -3,139 +3,411 @@
 
 systemDictionary = {
 	"ems-esp adapter settings": {
-		"en": "adapter settings for ems-esp",
-		"de": "Adaptereinstellungen für ems-esp"
+		"en": "ems-esp adapter settings",
+		"de": "ems-esp-Adaptereinstellungen",
+		"ru": "настройки адаптера ems-esp",
+		"pt": "configurações do adaptador ems-esp",
+		"nl": "instellingen voor ems-esp-adapter",
+		"fr": "paramètres de l'adaptateur ems-esp",
+		"it": "impostazioni dell'adattatore ems-esp",
+		"es": "configuración del adaptador ems-esp",
+		"pl": "ustawienia adaptera ems-esp",
+		"zh-cn": "ems-esp 适配器设置"
 	},
-	"Main settings":{
-		"en": "adapter settings for ems-esp",
-		"de": "Adaptereinstellungen für ems-esp"
+	"Main settings": {
+		"en": "Main settings",
+		"de": "Haupteinstellungen",
+		"ru": "Основные параметры",
+		"pt": "Configurações principais",
+		"nl": "Belangrijkste instellingen",
+		"fr": "Réglages principaux",
+		"it": "Impostazioni principali",
+		"es": "Ajustes principales",
+		"pl": "Ustawienia główne",
+		"zh-cn": "主要设置"
 	},
 	"Additional settings": {
 		"en": "Additional settings",
-		"de": "Zusätzliche Einstellungen"
+		"de": "Zusätzliche Einstellungen",
+		"ru": "Дополнительные настройки",
+		"pt": "Configurações adicionais",
+		"nl": "Aanvullende instellingen",
+		"fr": "Paramètres additionnels",
+		"it": "Altre impostazioni",
+		"es": "Ajustes adicionales",
+		"pl": "Dodatkowe ustawienia",
+		"zh-cn": "其他设置"
 	},
 	"km200_structure": {
 		"en": "km200 structure",
-		"de": "km200 Struktur"
+		"de": "km200 Struktur",
+		"ru": "Строение км200",
+		"pt": "estrutura km200",
+		"nl": "km200-structuur",
+		"fr": "km200 structure",
+		"it": "km200 struttura",
+		"es": "estructura km200",
+		"pl": "km200 struktura",
+		"zh-cn": "km200 结构"
 	},
 	"emsesp_active": {
-		"en": "EMSESP active",
-		"de": "EMSESP aktiv"
+		"en": "EMS-ESP active",
+		"de": "EMS-ESP aktiv",
+		"ru": "EMS-ESP активен",
+		"pt": "EMS-ESP ativo",
+		"nl": "EMS-ESP actief",
+		"fr": "EMS-ESP actif",
+		"it": "EMS-ESP attivo",
+		"es": "EMS-ESP activo",
+		"pl": "EMS-ESP aktywny",
+		"zh-cn": "EMS-ESP 活动"
 	},
 	"ems_http_wait": {
 		"en": "msec delay",
-		"de": "mSek delay"
+		"de": "Millisekunden Verzögerung",
+		"ru": "задержка в миллисекундах",
+		"pt": "atraso de mseg",
+		"nl": "msec vertraging",
+		"fr": "délai en millisecondes",
+		"it": "ritardo di msec",
+		"es": "retraso de milisegundos",
+		"pl": "Opóźnienie w milisekundach",
+		"zh-cn": "毫秒延迟"
 	},
 	"ems_dallas": {
 		"en": "read dallas",
-		"de": "Dallas lesen"
+		"de": "lese Dallas",
+		"ru": "читать Даллас",
+		"pt": "ler Dallas",
+		"nl": "lees dallas",
+		"fr": "lire dallas",
+		"it": "leggi dallas",
+		"es": "leer dallas",
+		"pl": "przeczytaj Dallas",
+		"zh-cn": "读达拉斯"
 	},
 	"ems_polling": {
 		"en": "polling in sec",
-		"de": "Polling in Sek"
+		"de": "Polling in Sek",
+		"ru": "опрос в секундах",
+		"pt": "votação em seg",
+		"nl": "polling in sec",
+		"fr": "sondage en secondes",
+		"it": "sondaggio in secondi",
+		"es": "sondeo en segundos",
+		"pl": "odpytywanie w sek",
+		"zh-cn": "秒轮询"
 	},
 	"emsesp_ip": {
 		"en": "IP address",
-		"de": "IP Adresse"
+		"de": "IP Adresse",
+		"ru": "айпи адрес",
+		"pt": "endereço de IP",
+		"nl": "IP adres",
+		"fr": "adresse IP",
+		"it": "indirizzo IP",
+		"es": "dirección IP",
+		"pl": "adres IP",
+		"zh-cn": "IP地址"
 	},
 	"ems_apiv3": {
 		"en": "use REST API V3",
-		"de": "verwende REST API V3"
+		"de": "REST-API V3 verwenden",
+		"ru": "использовать REST API V3",
+		"pt": "usar REST API V3",
+		"nl": "gebruik REST API V3",
+		"fr": "utiliser l'API REST V3",
+		"it": "usa l'API REST V3",
+		"es": "utilizar REST API V3",
+		"pl": "użyj REST API V3",
+		"zh-cn": "使用 REST API V3"
 	},
 	"ems_token": {
 		"en": "ems access token (V3 only)",
-		"de": "ems access token (nur V3)"
+		"de": "ems-Zugriffstoken (nur V3)",
+		"ru": "токен доступа ems (только V3)",
+		"pt": "token de acesso ems (apenas V3)",
+		"nl": "ems-toegangstoken (alleen V3)",
+		"fr": "jeton d'accès ems (V3 uniquement)",
+		"it": "token di accesso ems (solo V3)",
+		"es": "Token de acceso ems (solo V3)",
+		"pl": "token dostępu ems (tylko V3)",
+		"zh-cn": "ems 访问令牌（仅限 V3）"
 	},
 	"km200_active": {
 		"en": "KM200 active",
-		"de": "KM200 aktiv"
+		"de": "KM200 aktiv",
+		"ru": "KM200 активный",
+		"pt": "KM200 ativo",
+		"nl": "KM200 actief",
+		"fr": "KM200 actif",
+		"it": "KM200 attivo",
+		"es": "KM200 activo",
+		"pl": "KM200 aktywny",
+		"zh-cn": "KM200有源"
 	},
 	"km200_ip": {
 		"en": "IP address",
-		"de": "IP Adresse"
+		"de": "IP Adresse",
+		"ru": "айпи адрес",
+		"pt": "endereço de IP",
+		"nl": "IP adres",
+		"fr": "adresse IP",
+		"it": "indirizzo IP",
+		"es": "dirección IP",
+		"pl": "adres IP",
+		"zh-cn": "IP地址"
 	},
 	"km200_polling": {
 		"en": "polling in sec",
-		"de": "Polling in Sek"
+		"de": "Polling in Sek",
+		"ru": "опрос в секундах",
+		"pt": "votação em seg",
+		"nl": "polling in sec",
+		"fr": "sondage en secondes",
+		"it": "sondaggio in secondi",
+		"es": "sondeo en segundos",
+		"pl": "odpytywanie w sek",
+		"zh-cn": "秒轮询"
 	},
 	"control_file": {
 		"en": "csv-file for km200 | empty: no km200 data    *: read all km200 fields",
-		"de": "csv-file für km200 | leer: keine km200 Daten   *: alle km200 Felder)"
+		"de": "csv-Datei für km200 | leer: keine km200-Daten *: alle km200-Felder lesen",
+		"ru": "csv-файл для км200 | пусто: нет данных км200 *: прочитать все поля км200",
+		"pt": "arquivo csv para km200 | vazio: sem dados km200 *: ler todos os campos km200",
+		"nl": "csv-bestand voor km200 | leeg: geen km200-gegevens *: lees alle km200-velden",
+		"fr": "fichier csv pour km200 | vide : pas de données km200 * : lire tous les champs km200",
+		"it": "file csv per km200 | vuoto: nessun dato km200 *: leggi tutti i campi km200",
+		"es": "archivo csv para km200 | vacío: sin datos de km200 *: leer todos los campos de km200",
+		"pl": "plik csv dla km200 | puste: brak danych km200 *: odczytaj wszystkie pola km200",
+		"zh-cn": "km200 的 csv 文件 |空：没有 km200 数据 *：读取所有 km200 字段"
 	},
 	"gateway_pw": {
-		"en": "Gateway password",
-		"de": "Gerätepasswort"
+		"en": "gateway password",
+		"de": "Gateway-Passwort",
+		"ru": "пароль шлюза",
+		"pt": "senha de gateway",
+		"nl": "gateway wachtwoord",
+		"fr": "mot de passe passerelle",
+		"it": "password del gateway",
+		"es": "contraseña de puerta de enlace",
+		"pl": "hasło do bramy",
+		"zh-cn": "网关密码"
 	},
 	"private_pw": {
-		"en": "Private password",
-		"de": "Privates Passwort"
+		"en": "private password",
+		"de": "privates Passwort",
+		"ru": "личный пароль",
+		"pt": "senha privada",
+		"nl": "privé wachtwoord",
+		"fr": "Mot de passe privé",
+		"it": "password privata",
+		"es": "contraseña privada",
+		"pl": "prywatne hasło",
+		"zh-cn": "私人密码"
 	},
 	"recordings": {
-		"en": "power recordings (km200 only)",
-		"de": "Energie recordings (nur km200)"
+		"en": "energy recordings (km200 only)",
+		"de": "Energieaufzeichnungen (nur km200)",
+		"ru": "записи энергии (только 200 км)",
+		"pt": "registros de energia (apenas km200)",
+		"nl": "energieregistraties (alleen km200)",
+		"fr": "enregistrements énergétiques (km200 uniquement)",
+		"it": "rilevazioni energetiche (solo km200)",
+		"es": "registros de energía (solo km200)",
+		"pl": "zapisy energii (tylko km200)",
+		"zh-cn": "能量记录（仅限km200）"
 	},
 	"database_instance": {
 		"en": "db instance (mySQL or influxdb) for recordings & statistics",
-		"de": "DB Instanz (mySQL oder influxdb) für recordings & Statistik"
+		"de": "db-Instanz (mySQL oder influxdb) für Aufzeichnungen & Statistiken",
+		"ru": "экземпляр db (mySQL или Influxdb) для записи и статистики",
+		"pt": "instância db (mySQL ou influxdb) para gravações e estatísticas",
+		"nl": "db-instantie (mySQL of influxdb) voor opnames en statistieken",
+		"fr": "instance db (mySQL ou influxdb) pour les enregistrements et les statistiques",
+		"it": "db instance (mySQL o influxdb) per registrazioni e statistiche",
+		"es": "instancia de db (mySQL o influxdb) para grabaciones y estadísticas",
+		"pl": "instancja db (mySQL lub influxdb) do nagrań i statystyk",
+		"zh-cn": "用于记录和统计的数据库实例（mySQL 或 influxdb）"
 	},
 	"database": {
 		"en": "database name (iobroker)",
-		"de": "Datenbankname (iobroker)"
+		"de": "Datenbankname (iobroker)",
+		"ru": "имя базы данных (iobroker)",
+		"pt": "nome do banco de dados (iobroker)",
+		"nl": "databasenaam (iobroker)",
+		"fr": "nom de la base de données (iobroker)",
+		"it": "nome del database (iobroker)",
+		"es": "nombre de la base de datos (iobroker)",
+		"pl": "nazwa bazy danych (iobroker)",
+		"zh-cn": "数据库名称（iobroker）"
 	},
 	"eff_active": {
 		"en": "efficiency in %",
-		"de": "Wirkungsgrad in %"
+		"de": "Wirkungsgrad in %",
+		"ru": "КПД в%",
+		"pt": "eficiência em%",
+		"nl": "efficiëntie in %",
+		"fr": "efficacité en %",
+		"it": "efficienza in %",
+		"es": "eficiencia en%",
+		"pl": "wydajność w %",
+		"zh-cn": "效率（%）"
 	},
 	"eff20": {
-		"en": "boiler < 20°C",
-		"de": "HZG < 20°C"
+		"en": "< 20°C",
+		"de": "< 20°C",
+		"ru": "<20 ° С",
+		"pt": "<20 °C",
+		"nl": "< 20°C",
+		"fr": "< 20°C",
+		"it": "< 20°C",
+		"es": "<20 °C",
+		"pl": "<20°C",
+		"zh-cn": "< 20°C"
 	},
 	"eff25": {
-		"en": "boiler < 25°C",
-		"de": "HZG < 25°C"
+		"en": "< 25°C",
+		"de": "< 25°C",
+		"ru": "<25 ° С",
+		"pt": "<25 °C",
+		"nl": "< 25°C",
+		"fr": "< 25°C",
+		"it": "< 25°C",
+		"es": "<25 °C",
+		"pl": "< 25°C",
+		"zh-cn": "< 25°C"
 	},
 	"eff30": {
-		"en": "boiler < 30°C",
-		"de": "HZG < 30°C"
+		"en": "< 30°C",
+		"de": "< 30°C",
+		"ru": "<30 ° С",
+		"pt": "<30 °C",
+		"nl": "< 30°C",
+		"fr": "< 30°C",
+		"it": "< 30°C",
+		"es": "<30 °C",
+		"pl": "< 30°C",
+		"zh-cn": "< 30°C"
 	},
 	"eff35": {
-		"en": "boiler < 35°C",
-		"de": "HZG < 35°C"
+		"en": "< 35°C",
+		"de": "< 35°C",
+		"ru": "<35 ° С",
+		"pt": "<35 °C",
+		"nl": "< 35°C",
+		"fr": "< 35°C",
+		"it": "< 35°C",
+		"es": "<35 °C",
+		"pl": "<35°C",
+		"zh-cn": "< 35°C"
 	},
 	"eff40": {
-		"en": "boiler < 40°C",
-		"de": "HZG < 40°C"
+		"en": "< 40°C",
+		"de": "< 40°C",
+		"ru": "<40 ° С",
+		"pt": "<40 °C",
+		"nl": "< 40°C",
+		"fr": "< 40°C",
+		"it": "< 40°C",
+		"es": "<40 °C",
+		"pl": "<40°C",
+		"zh-cn": "< 40°C"
 	},
 	"eff45": {
-		"en": "boiler < 45°C",
-		"de": "HZG < 45°C"
+		"en": "< 45°C",
+		"de": "< 45°C",
+		"ru": "<45 ° С",
+		"pt": "<45 °C",
+		"nl": "< 45°C",
+		"fr": "< 45°C",
+		"it": "< 45°C",
+		"es": "<45 °C",
+		"pl": "<45°C",
+		"zh-cn": "< 45°C"
 	},
 	"eff50": {
-		"en": "boiler < 50°C",
-		"de": "HZG < 50°C"
+		"en": "< 50°C",
+		"de": "< 50°C",
+		"ru": "<50 ° С",
+		"pt": "<50 °C",
+		"nl": "< 50°C",
+		"fr": "< 50°C",
+		"it": "< 50°C",
+		"es": "<50 °C",
+		"pl": "< 50°C",
+		"zh-cn": "< 50°C"
 	},
 	"eff55": {
-		"en": "boiler < 55°C",
-		"de": "HZG < 55°C"
+		"en": "< 55°C",
+		"de": "< 55°C",
+		"ru": "<55 ° С",
+		"pt": "<55 °C",
+		"nl": "< 55°C",
+		"fr": "< 55°C",
+		"it": "< 55°C",
+		"es": "<55 °C",
+		"pl": "<55°C",
+		"zh-cn": "< 55°C"
 	},
 	"eff60": {
-		"en": "boiler < 60°C",
-		"de": "HZG < 60°C"
+		"en": "< 60°C",
+		"de": "< 60°C",
+		"ru": "<60 ° С",
+		"pt": "<60 °C",
+		"nl": "< 60°C",
+		"fr": "< 60°C",
+		"it": "< 60°C",
+		"es": "<60 °C",
+		"pl": "< 60°C",
+		"zh-cn": "< 60°C"
 	},
 	"eff70": {
-		"en": "boiler < 70°C",
-		"de": "HZG < 70°C"
+		"en": "< 70°C",
+		"de": "< 70°C",
+		"ru": "<70 ° С",
+		"pt": "<70 °C",
+		"nl": "< 70°C",
+		"fr": "< 70°C",
+		"it": "< 70°C",
+		"es": "<70 °C",
+		"pl": "<70°C",
+		"zh-cn": "< 70°C"
 	},
 	"syslog": {
 		"en": "syslog server active?",
-		"de": "Syslog Server aktiv?"
+		"de": "Syslog-Server aktiv?",
+		"ru": "сервер системного журнала активен?",
+		"pt": "servidor syslog ativo?",
+		"nl": "syslog-server actief?",
+		"fr": "serveur syslog actif ?",
+		"it": "server syslog attivo?",
+		"es": "servidor syslog activo?",
+		"pl": "serwer syslog jest aktywny?",
+		"zh-cn": "系统日志服务器活动？"
 	},
 	"syslog_port": {
 		"en": "syslog port",
-		"de": "Syslog Port"
+		"de": "Syslog-Port",
+		"ru": "порт системного журнала",
+		"pt": "porta syslog",
+		"nl": "syslog-poort",
+		"fr": "port syslog",
+		"it": "porta syslog",
+		"es": "puerto syslog",
+		"pl": "port dziennika systemowego",
+		"zh-cn": "系统日志端口"
 	},
 	"states_reorg": {
-		"en": "states reorg ",
-		"de": "States Reorg "
+		"en": "delete-states ",
+		"de": "Löschen-Zustände",
+		"ru": "удаленные состояния",
+		"pt": "delete-states",
+		"nl": "verwijder-statussen",
+		"fr": "supprimer-états",
+		"it": "elimina-stati",
+		"es": "eliminar-estados",
+		"pl": "usuwanie stanów",
+		"zh-cn": "删除状态"
 	}
 };
